e01529c8e759769c0eb45bf9f7586ec7c67cb265
pbiondo <biondo.a.peter@gmail.com>
2025-06-11 23:09:03 -0400

    Testing Images

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Important: this must match your GitHub repo name
+  base: '/twelfth-fret-music/', // Important: this must match your GitHub repo name
 });
